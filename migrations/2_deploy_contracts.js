fa08d75dc107b9bb006ed036b9f381dbe2a84e58
Mansuang Pawong <mansaung@hotmnail.com>
2021-06-13 10:52:14 +0700

    update config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Campaign = artifacts.require("./Campaign.sol");
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var CampaignFactory = artifacts.require("CampaignFactory");
+// var Campaign = artifacts.require("./Campaign.sol");
 
 module.exports = async (deployer) => {
   let addr = await web3.eth.getAccounts();
 
-  await deployer.deploy(SimpleStorage);
-  await deployer.deploy(Campaign, 100, addr[0]);
+  // await deployer.deploy(SimpleStorage);
+  // await deployer.deploy(Campaign, 100, addr[0]);
+  await deployer.deploy(CampaignFactory);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,21 @@
 const path = require("path");
+require("dotenv").config({filename: "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "nuxt/contracts"),
   networks: {
     develop: {
       port: 8545
+    },
+    ganache: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.URI, AccountIndex);
+      },
+      network_id: 5777
     }
   }
 };
